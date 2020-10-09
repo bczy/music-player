7029a9f0bfbf4379bdfb502f6e6bb298b2aee0d1
trambz <bertrand.coizy@gmail.com>
2020-10-09 13:13:07 +0200

    Adding spotify authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+
+import hash from "./utils/hash";
+
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 
 function App() {
+  
+  const [ token, setToken ] = useState('');
+  useEffect(()=>{
+    let _token = hash.access_token;
+
+    if (_token) {
+      // Set token
+      setToken(_token)
+      console.log(_token)
+    }
+  },[]);
   return (
     <div>
+      {!token && (
+            <a
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Please login to Spotify to access playlist
+            </a>
+          )}
     </div>
   );
 }
