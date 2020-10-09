7464e54de18aeb451fbef0c7e604387561ed10af
trambz <bertrand.coizy@gmail.com>
2020-10-09 17:55:19 +0200

    Adding currentTrack reducer

---
+++ src/store/reducers/currentTrack.js
@@ -0,0 +1,12 @@
+import * as types from '../actions/actionTypes'
+
+const currentTrack = (state = [] , action) => {
+  console.log(state,action)
+    switch (action.type) {
+      
+      default:
+        return state
+    }
+  }
+  
+  export default currentTrack;
