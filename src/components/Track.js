dbc1a543f713b16c624a7f932b797960cc2cee41
trambz <bertrand.coizy@gmail.com>
2020-10-09 16:47:48 +0200

    Adding track information

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 
 const Track = ({ onClick, track }) =>{ 
   console.log(track.track.artists[0]) 
-  console.log(track.track.name)
+  console.log(track.track.album.images)
   return (
   <li
     onClick={onClick}
   >
     <div>
+      <img src={track.track.album.images[2].url} alt=""/>
       <div>{track.track.name}</div>
       <div>{track.track.artists[0].name}</div>
     </div>
