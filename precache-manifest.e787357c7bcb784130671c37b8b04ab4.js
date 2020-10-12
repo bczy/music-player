85de37aee50786187f968dfa82f4381f2e50375a
trambz <bertrand.coizy@gmail.com>
2020-10-12 23:43:42 +0200

    Updates

---
+++ precache-manifest.e787357c7bcb784130671c37b8b04ab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0848ca44a8c73f62ecc511e18f07d41",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "c176684e67ef46d28d0f",
+    "url": "/music-player/static/js/2.bbd045f0.chunk.js"
+  },
+  {
+    "revision": "eaa35b1ccc0320d7ec9314f2e19ded2a",
+    "url": "/music-player/static/js/2.bbd045f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b7a85298fbb0ab8a06",
+    "url": "/music-player/static/js/main.02cd039e.chunk.js"
+  },
+  {
+    "revision": "ce993ddd42d9d92fac3a",
+    "url": "/music-player/static/js/runtime-main.68574bd7.js"
+  }
+]);
